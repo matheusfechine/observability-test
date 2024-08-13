94a9dcd783b97e97d284efe6466c671063a0b725
Matheus Fechine de Moura <matheus_fechine@virgo.atlantico.net.br>
2024-08-13 18:16:47 -0300

    Some fixes

--- src/main/java/br/com/performance/observability/ObservabilityPerformanceApplication.java
+++ src/main/java/br/com/performance/observability/ObservabilityPerformanceApplication.java
@@ -1,10 +1,18 @@
 package br.com.performance.observability;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ObservabilityPerformanceApplication {
+	@Bean
+	public ObservedAspect observedAspect(ObservationRegistry observationRegistry){
+		return new ObservedAspect(observationRegistry);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ObservabilityPerformanceApplication.class, args);
 	}
--- src/main/java/br/com/performance/observability/repository/UserRepository.java
+++ src/main/java/br/com/performance/observability/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.performance.observability.repository;
 
 import br.com.performance.observability.model.User;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Observed(name = "UserRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
